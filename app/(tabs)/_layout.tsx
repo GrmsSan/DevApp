bad91b98294f16fb1b41db3737199e1f40992b27
GrmsSan <gugubriel15@gmail.com>
2025-05-12 16:43:55 -0300

    Add files via upload

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Text } from 'react-native';
+
+
+export default function TabLayout() {
+
+  return (
+    <Tabs>
+      <Tabs.Screen name='index' options={{headerShown: false, tabBarIcon: () => (<Text>👌</Text>) }}></Tabs.Screen>
+      <Tabs.Screen name='explore' options={{headerShown: true, tabBarIcon: () => (<Text>❤</Text>) }}></Tabs.Screen>
+      <Tabs.Screen name='list' options={{headerShown: true, tabBarIcon: () => (<Text>👍</Text>) }}></Tabs.Screen>
+    </Tabs>
+  );
+}
